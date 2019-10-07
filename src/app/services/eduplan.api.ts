f32c6e5466d101eb6dddba4ab8b4699fbe1842a9
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-10-07 17:04:47 -0300

    alterando a API para o heroku

--- src/app/services/eduplan.api.ts
+++ src/app/services/eduplan.api.ts
@@ -1,1 +1,2 @@
-export const EDUPLAN_API: string = 'http://localhost:9090';
+// export const EDUPLAN_API: string = 'http://localhost:9090';
+export const EDUPLAN_API: string = 'https://app-eduplan.herokuapp.com';
