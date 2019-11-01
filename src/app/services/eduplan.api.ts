7767a3e7e244c72d1c9bdb3fb6169fd43cb2fde1
Jonathan Ramos Nascimento <ramos.was.jonathan@gmail.com>
2019-11-01 12:41:14 -0300

    alterando link da API

--- src/app/services/eduplan.api.ts
+++ src/app/services/eduplan.api.ts
@@ -1,2 +1,2 @@
 // export const EDUPLAN_API: string = 'http://localhost:9090';
-export const EDUPLAN_API: string = 'https://app-eduplan.herokuapp.com';
+export const EDUPLAN_API: string = 'http://localhost:9090';
